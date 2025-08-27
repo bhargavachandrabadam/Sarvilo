6d50ca88474599c527311a9c006269167bd471c2
bhargavachandrabadam <bhargavachandrabadam@gmail.com>
2025-08-27 21:57:35 +0530

    added sarvilo spring intializr

---
+++ src/main/java/com/bbc/sarvilo/SarviloApplication.java
@@ -0,0 +1,13 @@
+package com.bbc.sarvilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarviloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SarviloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bbc/sarvilo/SarviloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbc.sarvilo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarviloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
